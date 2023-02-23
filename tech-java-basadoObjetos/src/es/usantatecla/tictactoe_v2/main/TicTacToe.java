653fecf36cff3b49d2a126d191012b76048de6c7
xavi gomez <jumpering@gmail.com>
2023-02-23 21:24:32 +0100

    delete TicTacToeView  UML

--- tech-java-basadoObjetos/src/es/usantatecla/tictactoe_v2/main/TicTacToe.java
+++ tech-java-basadoObjetos/src/es/usantatecla/tictactoe_v2/main/TicTacToe.java
@@ -1,34 +1,47 @@
 package es.usantatecla.tictactoe_v2.main;
 
 import es.usantatecla.tictactoe_v2.main.models.Board;
+import es.usantatecla.tictactoe_v2.main.models.Message;
 import es.usantatecla.tictactoe_v2.main.models.Turn;
-import es.usantatecla.tictactoe_v2.main.views.TicTacToeView;
+import es.usantatecla.tictactoe_v2.main.views.BoardView;
+import es.usantatecla.tictactoe_v2.main.views.TurnView;
+import es.usantatecla.tictactoe_v2.utils.YesNoDialogView;
 
 class TicTacToe {
 
 	private Board board;
 	private Turn turn;
-	private TicTacToeView ticTacToeView;
+	private BoardView boardView;
+	private TurnView turnView;
 
 	private TicTacToe() {
 		this.board = new Board();
 		this.turn = new Turn(this.board);
-		this.ticTacToeView = new TicTacToeView(this.turn, this.board);
+		this.boardView = new BoardView(this.board);
+		this.turnView = new TurnView(this.turn);
 	}
 
 	private void play() {
 		do {
 			this.playGame();
-		} while (this.ticTacToeView.isResumedGame());
+		} while (this.isResumedGame());
 	}
 
 	private void playGame() {
 		this.board.reset();
 		this.turn.reset();
 		do {
-			this.ticTacToeView.play();
+			this.boardView.write();
+			this.turnView.play();
 		} while (!this.board.isTicTacToe(this.turn.getActiveColor()));
-		this.ticTacToeView.writeWinner();
+		this.boardView.write();
+		this.turnView.writeWinner();
+	}
+
+	public boolean isResumedGame() {
+		YesNoDialogView yesNoDialog = new YesNoDialogView();
+		yesNoDialog.read(Message.RESUME.toString());
+		return yesNoDialog.isAffirmative();
 	}
 
 	public static void main(String[] args) {
